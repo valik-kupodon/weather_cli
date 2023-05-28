22e8c57aa80759b87518d05b455a1c3f837fcc28
vfilatov <v.filatov@andersenlab.com>
2023-05-28 15:44:16 +0300

    Implemented base CLI commands

--- src/command_line_interface/mod.rs
+++ src/command_line_interface/mod.rs
@@ -1,34 +1,67 @@
-use clap:: {
-    Args,
-    Parser,
-    Subcommand
-};
+use clap::{Arg, ArgMatches, Command};
 
-#[derive(Debug, Parser)]
-#[clap(author, version, about)]
-pub struct WeatherClIArgs{
-    #[clap(subcommand)]
-    pub weather_integration_type: Option<WeatherIntegrationType>,
+pub fn get_args() {
+    let matches = register_args();
+
+    // Matches the commands and performs actions
+    match matches.subcommand() {
+        Some(("configure", sub_m)) => {
+            if sub_m.contains_id("provider") {
+                let packages: Vec<_> = sub_m
+                    .get_many::<String>("provider")
+                    .expect("contains_id")
+                    .map(|s| s.as_str())
+                    .collect();
+                let values = packages.join(", ");
+                println!("Configuring provider: {}", values);
+            }
+            // TODO: Implement the configuration logic here
+        },
+        Some(("get", sub_m)) => {
+            if sub_m.contains_id("address") {
+                let packages: Vec<_> = sub_m
+                    .get_many::<String>("address")
+                    .expect("contains_id")
+                    .map(|s| s.as_str())
+                    .collect();
+                let address = packages.join(", ");
+                println!("Configuring provider: {}", address);
+            }
+            if sub_m.contains_id("date") {
+                let packages: Vec<_> = sub_m
+                    .get_many::<String>("date")
+                    .expect("contains_id")
+                    .map(|s| s.as_str())
+                    .collect();
+                let date = packages.join(", ");
+                println!("Configuring provider: {}", date);
+            }
+            // TODO: Implement the weather fetching logic here
+        },
+        _ => (),
+    }
 }
 
-#[derive(Debug, Subcommand)]
-pub enum  WeatherIntegrationType {
-    Configure(OpenWeatherMapConfigure),
-    GET(LocationAndTime)
-}
-
-#[derive(Debug, Args)]
-pub struct OpenWeatherMapConfigure{
-    /// Coordinates in format <lan,lon>
-    coordinates: String,
-    /// Api key from https://openweathermap.org
-    api_key: String
-}
-
-#[derive(Debug, Args)]
-pub struct LocationAndTime{
-    /// Coordinates in format <lan,lon>
-    coordinates: Option<String>,
-    /// Date if none was pass it will use current
-    date: Option<String>
+pub fn register_args() -> ArgMatches {
+    Command::new("Weather CLI")
+        .version("1.0")
+        .author("Your Name valentinefilatov2015@gmail.com")
+        .about("Shows weather to a user")
+        .subcommand(Command::new("configure")
+            .about("Configures credentials for a provider")
+            .arg(Arg::new("provider")
+                .help("Sets the provider to configure")
+                .required(true)
+                .index(1)))
+        .subcommand(Command::new("get")
+            .about("Shows weather for the provided address")
+            .arg(Arg::new("address")
+                .help("Sets the address to get weather for")
+                .required(true)
+                .index(1))
+            .arg(Arg::new("date")
+                .help("Sets the date (optional, defaults to now)")
+                .required(false)
+                .index(2)))
+        .get_matches()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 mod command_line_interface;
 
-use clap::Parser;
-use command_line_interface::WeatherClIArgs;
-
 fn main() {
-    let args = WeatherClIArgs::parse();
-    println!("Hello world!")
+    command_line_interface::get_args()
 }
